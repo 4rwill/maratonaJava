bea30e4473616f7ca6a5d8cb1c9c5ec3a6263920
4rwill <christian10will@gmail.com>
2025-03-04 13:30:02 -0300

    Arquivos iniciais

---
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        int n1 = 10;
+        int n2 = 20;
+        System.out.println("Valor: "+(n1+n2));
+        //Usa esse pararênteses pra não misturar o + de soma com o + de concatenação
+    }
+}
